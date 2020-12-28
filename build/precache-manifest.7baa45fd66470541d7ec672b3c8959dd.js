24d3918eefdea30933570efecfeb498187998f70
rachejazz <divyadeepti2000@gmail.com>
2020-12-29 01:45:23 +0530

    build dir check

---
+++ build/precache-manifest.7baa45fd66470541d7ec672b3c8959dd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52798ba87e5625b81951a383f8e86d7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "081b1895a73fb9ff4aa3",
+    "url": "/static/css/main.aa20a36f.chunk.css"
+  },
+  {
+    "revision": "a1aff45fff8ecab2d69d",
+    "url": "/static/js/2.2e6c938b.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "/static/js/2.2e6c938b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "081b1895a73fb9ff4aa3",
+    "url": "/static/js/main.99ce8224.chunk.js"
+  },
+  {
+    "revision": "ea630fe81389fbceefbb",
+    "url": "/static/js/runtime-main.bf9f4589.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "8e35a69279e2fb413b96b999c9783c9e",
+    "url": "/static/media/features-split-image-01.8e35a692.png"
+  },
+  {
+    "revision": "af74bd04e3c8efe9ec36851e36acdf6b",
+    "url": "/static/media/features-split-image-02.af74bd04.png"
+  },
+  {
+    "revision": "0dee93fa2c8c6672251eb1ac60c92d0b",
+    "url": "/static/media/illustration-section-01.0dee93fa.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "3ad960082282fd9db4c1e36113475c18",
+    "url": "/static/media/logo.3ad96008.svg"
+  },
+  {
+    "revision": "413246b04c071cb9a5629e30a379b1c0",
+    "url": "/static/media/video-placeholder.413246b0.jpg"
+  }
+]);
